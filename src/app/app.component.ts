5700801265d135aba9415c107a5deaa9d12735f5
Cristian Amador <crissis2004@gmail.com>
2022-09-25 13:30:52 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'SaberMedidas';
+export class AppComponent implements OnInit {
+  width = 0;
+  height = 0;
+  ngOnInit(): void {
+    this.width = window.screen.width;
+    this.height = window.screen.height;
+  }
+
 }
