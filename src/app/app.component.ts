ed5e872258f7848cf28597cd1f157379c4d2bff7
Cristian Amador <crissis2004@gmail.com>
2022-10-02 18:08:20 -0500

    Actualice estilos con glassmorphism y jquey outumn leaf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var jQuery: any;
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,6 +13,16 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.width = window.screen.width;
     this.height = window.screen.height;
+    $.AutumnLeafStart({
+      leavesfolder: "../assets/AutumnLeaves/",
+      howmanyimgsare: 8,
+      initialleaves: 20,
+      maxyposition: -10,
+      multiplyclick: true,
+      multiplynumber: 3,
+      infinite: true,
+      fallingsequence: 4000
+    })
   }
 
 }
